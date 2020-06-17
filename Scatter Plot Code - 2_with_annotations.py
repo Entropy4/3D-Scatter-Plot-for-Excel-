e38faeabbbf11b6ac7bebc2e1a9d86063489a04e
Entropy4 <52324356+Entropy4@users.noreply.github.com>
2020-06-17 12:56:34 +0530

    Update Scatter Plot Code - 2_with_annotations.py

--- Scatter Plot Code - 2_with_annotations.py
+++ Scatter Plot Code - 2_with_annotations.py
@@ -17,9 +17,9 @@ loc=(r"D:\User\Desktop\Suja\AIRGLOW-MAY-2020\AG-WORK-SHIVA.xls")
 sheet_pos=3                                      #sheet position number
 start_row=2                                      #starting row number
 end_row=98                                       #ending row number
-sa_colindex=11                                   #column no. zero index of Sun Angle
-sf_colindex=12                                   #column no. zero index of Solar Flux
-ca_colindex=13                                   #column no. zero index of FUV_TEC_MIN
+x_colindex=11                                   #column no. zero index of X data
+y_colindex=12                                   #column no. zero index of Y data
+z_colindex=13                                   #column no. zero index of Z data
 ##
 
 i_min=start_row-1
@@ -39,9 +39,9 @@ Y=[]
 Z=[]
 
 for i in range(i_min,i_max):
-    itemx=sheet.cell_value(i,sa_colindex)                 
-    itemy=sheet.cell_value(i,sf_colindex)
-    itemz=sheet.cell_value(i,ca_colindex)
+    itemx=sheet.cell_value(i,x_colindex)                 
+    itemy=sheet.cell_value(i,y_colindex)
+    itemz=sheet.cell_value(i,z_colindex)
     X.append(itemx)
     Y.append(itemy)
     Z.append(itemz)
@@ -50,9 +50,9 @@ for i in range(i_min,i_max):
 #code to display the scatter plot
 ax.scatter3D(X, Y, Z, c=Z,cmap='inferno')
 
-ax.set_xlabel("Sun Angle")
-ax.set_ylabel("Solar Flux")
-ax.set_zlabel("Constant Airglow");
+ax.set_xlabel("X Data")
+ax.set_ylabel("Y Data")
+ax.set_zlabel("Z Data");
 
 plt.show()
 plt.close('all')
